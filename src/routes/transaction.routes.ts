ea827a5f358e689e0776c8841b241b86c2d9acc4
amaralc <amaral.calil@gmail.com>
2020-12-14 23:57:32 -0300

    FEATURE / Define routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,47 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+/** Create instance of repository */
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    /** Get all transactions */
+    const transactions = transactionsRepository.all();
+
+    /** Get balance */
+    const balance = transactionsRepository.getBalance();
+
+    /** Return transactions and balance */
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+transactionRouter.post('/', async (request, response) => {
   try {
-    // TODO
+    /** Create instance of service */
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    /** Get request data */
+    const { title, type, value } = request.body;
+
+    /** Execute service to create transaction */
+    const transaction = await createTransactionService.execute({
+      title,
+      type,
+      value,
+    });
+
+    /** Return transaction */
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
